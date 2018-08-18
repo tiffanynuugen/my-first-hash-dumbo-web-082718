831c2a423fa131209f03065b8753f27c77ac00a6
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-18 20:23:11 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -38,6 +38,6 @@ def adding
 
 
   # return the shipping_manifest hash below
-  return shipping_manifest = {}
+  return shipping_manifest
 
 end
